fb8d9f59a9d03de3400fc6cf9cbc8092fe54d47f
EWRGroeneveld <Saiyato@users.noreply.github.com>
2018-12-11 13:07:23 +0100

    Create ovpnclient.en.pl

---
+++ ovpnclient.en.pl
@@ -0,0 +1,15 @@
+# Added for OpenVPN client
+%tr = (
+%tr,
+'ovpnclient' => 'OpenVPN Client UI',
+'ovpnclient config' => 'Configuration',
+'ovpnclient use client' => 'Enable OpenVPN client',
+'ovpnclient tcp' => 'TCP',
+'ovpnclient udp' => 'UDP',
+'ovpnclient table' => 'Table',
+'ovpnclient chain' => 'Chain',
+'ovpnclient source' => 'Source',
+'ovpnclient destination' => 'Destination',
+'ovpnclient protocol' => 'Protocol',
+'ovpnclient action' => 'Action'
+);
